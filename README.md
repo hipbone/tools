9088b836bb34ec3a14b75418bf6ca2c809956456
Hyun Namgung <hyun996@gmail.com>
2024-02-16 15:47:23 +0900

    서비스 및 서버 이전 체크 스크립트 추가

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 시스템 운영 시 필요한 다양한 도구를 모아 놓는다.
 
+## 파이썬 환경 - 공통
+
+### Ubuntu & Debian
+
+```bash
+python3.x -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
 ## genpasswd.py
 
 비밀번호 생성시
@@ -14,4 +24,23 @@
 
 ```bash
 python genpasswd.py
+```
+
+## migration_check.py
+
+서버 또는 서비스 이전 시 도메인, 방화벽, 서비스 환경 구성을 확인하기 위한 스크립트
+
+### 환경
+
+* lang : python 3.12
+
+### 사용법
+
+```bash
+# migration_check.py 파일에 사용자 변수 설정 
+file_path = "list"  # 파일명
+hc_path = "/check"  # 테스트 호출할 URL의 Path
+ssl_verify = False  # SSL 인증서 검증을 할지 여부 - 서비스 도메인의 경우 옵션을 활성화해서 인증서도 검증한다.
+
+python migration_check.py
 ```
---
+++ migraton_check.py
@@ -0,0 +1,93 @@
+import socket
+import http.client
+import ssl
+from colorama import init, Fore
+
+# variables
+file_path = "url_list"
+hc_path = "/check"
+ssl_verify = False
+
+# colorama 초기화
+init(autoreset=True)
+
+
+def is_ip_address(value):
+    try:
+        socket.inet_aton(value)
+        return True
+    except socket.error:
+        return False
+
+
+def check_dns(input_value):
+    try:
+        ip_address = socket.gethostbyname(input_value)
+        print(Fore.GREEN + f"[Success] DNS Lookup: {input_value} / {ip_address}")
+        return input_value, ip_address
+    except socket.error as e:
+        print(Fore.RED + f"[Error] DNS Lookup Failed: {e}")
+        return None
+
+
+def check_http(domain, port):
+    try:
+        # HTTP 연결 시도
+        conn = http.client.HTTPConnection(domain, port, timeout=5)
+        conn.request("GET", hc_path)
+        response = conn.getresponse()
+
+        if response.status in [200, 301, 302]:
+            print(Fore.GREEN + f"[Success] HTTP Test: {response.status}")
+        else:
+            print(Fore.RED + f"[Error] HTTP Test Failed: {response.status}")
+
+        conn.close()
+    except Exception as e:
+        print(Fore.RED + f"[Error] HTTP Test Failed: {e}")
+
+
+def check_https(domain, port):
+    try:
+        # HTTPS 연결 시도
+        context = ssl.create_default_context()
+        if not ssl_verify:
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+
+        conn = http.client.HTTPSConnection(domain, port, context=context, timeout=5)
+        conn.request("GET", hc_path)
+        response = conn.getresponse()
+
+        if response.status == 200:
+            print(Fore.GREEN + f"[Success] HTTPS Test: {response.status}")
+        else:
+            print(Fore.RED + f"[Error] HTTPS Test Failed: {response.status}")
+
+        conn.close()
+    except Exception as e:
+        print(Fore.RED + f"[Error] HTTPS Test Failed: {e}")
+
+
+if __name__ == "__main__":
+    try:
+        with open(file_path, "r") as file:
+            for line in file.readlines():
+                line = line.strip()
+                print(f"===== {line} check =====")
+                if is_ip_address(line):
+                    # 1. DNS Lookup
+                    print(Fore.YELLOW + f"Skip DNS Lookup : {line}")
+                else:
+                    # 1. DNS Lookup
+                    domain, ip_address = check_dns(line)
+                # 2. HTTP 연결 테스트
+                check_http(line, 80)
+                # 3. HTTPS 연결 테스트
+                check_https(line, 443)
+                print(f"")
+
+    except FileNotFoundError:
+        print(Fore.RED + f"파일을 찾을 수 없습니다 : {file_path}")
+    except Exception as e:
+        print(Fore.RED + f"에러가 발생 했습니다 : {e}")
